e0b1f4319d7980d4821979f8d51e3247d4f35ac9
Mumin Bhat <mumin.fayaz5050@gmail.com>
2023-04-01 16:51:46 +0530

    contact form added

--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contact
 
 # Create your views here.
 
 def home(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        contact = Contact(name=name, email=email, message=message)
+        contact.save()
+
     return render(request, 'index.html')
